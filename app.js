6658f6d1acdf91d90b62c7e8faee16f1daf2a027
aaishwaryamali02-a11y <aaishwaryamali02@gmail.com>
2026-09-13 23:17:23 +0530

    Fix home route

--- app.js
+++ app.js
@@ -68,9 +68,10 @@ saveUninitialized: true,
   },
 };
 
-/* app.get("/", (req, res) => {
-  res.send("Hi, I am root");
-}); */
+  app.get("/", (req, res) => {
+  res.redirect("/listings");
+});
+
 
 
 app.use(session(sessionOptions));
